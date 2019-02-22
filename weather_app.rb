4941a425ec7668200511d69ffe81971f1835bb5b
Peter Jang <peter.x.jang@gmail.com>
2019-02-21 19:11:22 -0600

    Initial commit

---
+++ weather_app.rb
@@ -0,0 +1,12 @@
+require "http"
+
+system "clear"
+puts "Welcome to the weather app!"
+print "Enter a city: "
+city = gets.chomp
+
+response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&APPID=______")
+weather_data = response.parse
+temperature = weather_data["main"]["temp"]
+description = weather_data["weather"][0]["description"]
+puts "Today in #{city} it is #{temperature} degrees outside with #{description}."
